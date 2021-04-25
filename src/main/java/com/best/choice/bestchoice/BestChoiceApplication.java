2abebf086e3dde471659910dc267ceb7a35f0522
Tiago Oliveira <tiagooliveira1409@gmail.com>
2021-04-25 18:22:18 -0300

    Super class

---
+++ src/main/java/com/best/choice/bestchoice/BestChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.best.choice.bestchoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestChoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestChoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/best/choice/bestchoice/BestChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.best.choice.bestchoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestChoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
